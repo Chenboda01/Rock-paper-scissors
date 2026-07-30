cae80c01becd6bd4a99dde8faa8172616f6fc24e
Boda Chen <chenboda01>
2026-07-30 15:49:14 -0400

    Add rock paper scissors game

---
+++ rock-paper-scissor.py
@@ -0,0 +1,36 @@
+import random
+
+choices = ("r", "p", "s")
+
+while True:
+    user_choice = input(
+        "Rock, paper, scissors? (r/p/s): "
+    ).lower().strip()
+
+    if user_choice not in choices:
+        print("Invalid choice!")
+        continue
+
+    computer_choice = random.choice(choices)
+
+    print(f"You chose {user_choice}")
+    print(f"Computer chose {computer_choice}")
+
+    if user_choice == computer_choice:
+        print("Tie!")
+
+    elif (
+        (user_choice == "r" and computer_choice == "s")
+        or (user_choice == "s" and computer_choice == "p")
+        or (user_choice == "p" and computer_choice == "r")
+    ):
+        print("You win!")
+
+    else:
+        print("You lose!")
+
+    should_continue = input("Resume? (y/n): ").lower().strip()
+
+    if should_continue == "n":
+        print("Thanks for playing!")
+        break
